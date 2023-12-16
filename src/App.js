998000dbee148790fc9124db8d4bc75c70af6205
Korjubzot <korjubzot@gmail.com>
2023-12-16 16:04:30 +0100

    Basic Supabase setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,49 @@
 import "./App.css";
-import logo from "./logo2.svg";
+
+import { useState, useEffect } from "react";
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  process.env.REACT_APP_SUPABASE_URL,
+  process.env.REACT_APP_SUPABASE_API_KEY
+);
 
 function App() {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  async function getRecipes() {
+    const { data } = await supabase.from("recipes").select();
+    console.log(data);
+    setRecipes(data);
+  }
+
+  async function addRecipe() {
+    const newRecipe = { name: "Testing...", cuisine: "American" };
+    const { data, error } = await supabase.from("recipes").insert([newRecipe]);
+
+    if (error) {
+      console.error("Error adding new recipe:", error);
+    } else if (data) {
+      setRecipes([...recipes, data[0]]);
+    } else {
+      console.error("No data returned after inserting new recipe");
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hi handsome</h1>
-      </header>
+      <button onClick={addRecipe}>Add Recipe</button>
+      <ul>
+        {recipes.map((recipe) => (
+          <li key={recipe.name}>
+            {recipe.name}, {recipe.cuisine}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
