3421e4ea3cd22ed9e6298f630588dcd58bf9c8b5
Korjubzot <korjubzot@gmail.com>
2023-12-16 16:35:42 +0100

    Supabase auth basics added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 
 import { useState, useEffect } from "react";
 import { createClient } from "@supabase/supabase-js";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
 
 const supabase = createClient(
   process.env.REACT_APP_SUPABASE_URL,
@@ -9,6 +11,7 @@ const supabase = createClient(
 );
 
 function App() {
+  const [session, setSession] = useState(null);
   const [recipes, setRecipes] = useState([]);
 
   useEffect(() => {
@@ -34,18 +37,36 @@ function App() {
     }
   }
 
-  return (
-    <div className="App">
-      <button onClick={addRecipe}>Add Recipe</button>
-      <ul>
-        {recipes.map((recipe) => (
-          <li key={recipe.name}>
-            {recipe.name}, {recipe.cuisine}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  if (!session) {
+    return <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />;
+  } else
+    return (
+      <div className="App">
+        <button onClick={addRecipe}>Add Recipe</button>
+        <ul>
+          {recipes.map((recipe) => (
+            <li key={recipe.name}>
+              {recipe.name}, {recipe.cuisine}
+            </li>
+          ))}
+        </ul>
+        <button onClick={() => supabase.auth.signOut()}>Sign Out</button>
+      </div>
+    );
 }
 
 export default App;
